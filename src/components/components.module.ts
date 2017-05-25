5d698a64926bdea08444786dc61957f167d92f63
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2017-05-26 00:00:24 +0100

    added nested component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,9 +3,12 @@ import { MusicCardComponent } from './music-card/music-card'
 import { MusicItemComponent } from './music-item/music-item'
 import {IonicModule}  from 'ionic-angular'
 import { PipesModule } from '../pipes/pipes.module'
+import { PopoverComponent } from './popover/popover'
+
 @NgModule({
-  declarations: [MusicCardComponent, MusicItemComponent],
+  declarations: [MusicCardComponent, MusicItemComponent, PopoverComponent],
   imports: [PipesModule, IonicModule],
-  exports: [MusicCardComponent, MusicItemComponent]
+  exports: [MusicCardComponent, MusicItemComponent, PopoverComponent],
+  // entryComponents: [PopoverComponent]
 })
 export class ComponentsModule { }
--- src/components/music-item/music-item.ts
+++ src/components/music-item/music-item.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { PopoverComponent } from '../popover/popover'
+import { PopoverController } from 'ionic-angular';
 @Component({
   selector: 'music-item',
   templateUrl: 'music-item.html'
 })
 export class MusicItemComponent {
   @Input() track
-  constructor() {}
-
+  constructor(private popoverCtrl: PopoverController) {
+    this.popoverCtrl.create(PopoverComponent).present();
+  }
 }
