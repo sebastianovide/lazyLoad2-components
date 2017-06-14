85fa20199feaa07453162b801640efe14ec0f526
Sebastian Ovide <Sebastian.Ovide@geovation.uk>
2017-06-14 17:05:09 +0100

    added a "hello" component that opens a pop over

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,9 +3,13 @@ import { MusicCardComponent } from './music-card/music-card'
 import { MusicItemComponent } from './music-item/music-item'
 import {IonicModule}  from 'ionic-angular'
 import { PipesModule } from '../pipes/pipes.module'
+import { PopoverComponent } from './popover/popover'
+import { HelloComponent } from "./hello/hello";
+
 @NgModule({
-  declarations: [MusicCardComponent, MusicItemComponent],
+  declarations: [MusicCardComponent, MusicItemComponent, PopoverComponent, HelloComponent],
   imports: [PipesModule, IonicModule],
-  exports: [MusicCardComponent, MusicItemComponent]
+  exports: [MusicCardComponent, MusicItemComponent, PopoverComponent, HelloComponent],
+  // entryComponents: [PopoverComponent]
 })
 export class ComponentsModule { }
